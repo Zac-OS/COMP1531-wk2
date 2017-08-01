819769c6b23feaaa1bf7175c4a4014ed9c52f76f
Zac Partridge <z5165457@cse.unsw.edu.au>
2017-08-01 11:14:44 +1000

    changed fizz buzz

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,6 +1,9 @@
-n = input("Enter number: ")
 for i in range(100):
-    if i%n == 0:
+    if i%3 == 0 and i%5 == 0:
+        print("FIzz Buzz")
+    elif i%5 == 0:
         print("Buzz")
+    elif i%3 == 0:
+        print ("Fizz")
     else:
         print(i)
